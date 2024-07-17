c0738226d0f2d645c5272468cc1ce73e206ea04d
Takuya Kosugiyama <re@itkq.jp>
2024-07-17 17:32:58 +0900

    scripts: omit logging

--- scripts/deploy-config.sh
+++ scripts/deploy-config.sh
@@ -8,14 +8,10 @@ pwd="$(pwd)"
 
 for p in $(find ${pwd}/config -mindepth 2 -maxdepth 2 -type d); do
 	dir=$(basename "$p")
-	set -x
 	./scripts/symlink.sh "${pwd}/config/.config/${dir}" "$HOME/.config/${dir}"
-	{ set +x; } 2>/dev/null
 done
 
 for p in $(find ${pwd}/config -mindepth 1 -maxdepth 1 -type f); do
 	file=$(basename "$p")
-	set -x
 	./scripts/symlink.sh "${pwd}/config/${file}" "$HOME/${file}"
-	{ set +x; } 2>/dev/null
 done
