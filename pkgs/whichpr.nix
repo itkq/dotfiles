657b4d95cf716c0e6bbff98045a7c76299e69f41
Takuya Kosugiyama <re@itkq.jp>
2024-07-24 20:54:42 +0900

    pkg: fix whichpr

--- pkgs/whichpr.nix
+++ pkgs/whichpr.nix
@@ -1,23 +1,32 @@
 { pkgs }:
 
 pkgs.stdenv.mkDerivation {
-  name = "aqua";
-  src = pkgs.fetchurl {
-    # XXX: darwin arm64 not distributed
-    url = if pkgs.stdenv.isDarwin then "https://github.com/pocke/whichpr/releases/download/v1.0.0/whichpr_1.0.0_darwin_amd64.tar.gz" else "https://github.com/pocke/whichpr/releases/download/v1.0.0/whichpr_1.0.0_linux_amd64.tar.gz";
-    sha256 = if pkgs.stdenv.isDarwin then pkgs.lib.fakeSha256 else pkgs.lib.fakeSha256;
+  pname = "whichpr";
+  version = "1.0.0";
+
+  src = pkgs.fetchFromGitHub {
+    owner = "itkq";
+    repo = "whichpr";
+    rev = "master";
+    sha256 = "EA0+w05Ks2O23wEbdVxs3ICv7hrQrkRnCXyykZkazRg=";
   };
 
-  sourceRoot = ".";
+  buildInputs = [ pkgs.go ];
+
+  buildPhase = ''
+    export GOCACHE=$(mktemp -d)
+    export GOMODCACHE=$(mktemp -d)
+    go build -o whichpr .
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp -r whichpr $out/bin
+    cp whichpr $out/bin/
   '';
 
   meta = {
     description = "Find the pull request from commit hash.";
-    homepage = "https://github.com/pocke/whichpr";
+    homepage = "https://github.com/itkq/whichpr";
     license = pkgs.lib.licenses.asl20;
   };
 }
