7659b26f51f13398c9e5bdbf63a1679c0ebdff61
Takuya Kosugiyama <re@itkq.jp>
2024-07-24 20:29:57 +0900

    pkgs: case linux

--- pkgs/aqua.nix
+++ pkgs/aqua.nix
@@ -1,12 +1,12 @@
-# https://github.com/izumin5210/dotfiles/blob/d9c5895326f03156944a463ff89177738db29902/pkgs/aqua.nix
+# based on https://github.com/izumin5210/dotfiles/blob/d9c5895326f03156944a463ff89177738db29902/pkgs/aqua.nix
 
 { pkgs }:
 
 pkgs.stdenv.mkDerivation {
   name = "aqua";
   src = pkgs.fetchurl {
-    url = "https://github.com/aquaproj/aqua/releases/download/v2.27.0/aqua_darwin_arm64.tar.gz";
-    sha256 = "sha256-z0UcCsSqi4Q7OhOyDENLLjD8dnfo4oKQcUx1Ny/q0qs=";
+    url = if pkgs.stdenv.isDarwin then "https://github.com/aquaproj/aqua/releases/download/v2.27.0/aqua_darwin_arm64.tar.gz" else "https://github.com/aquaproj/aqua/releases/download/v2.27.0/aqua_linux_amd64.tar.gz";
+    sha256 = if pkgs.stdenv.isDarwin then "sha256-z0UcCsSqi4Q7OhOyDENLLjD8dnfo4oKQcUx1Ny/q0qs=" else pkgs.lib.fakeSha256;
   };
 
   sourceRoot = ".";
