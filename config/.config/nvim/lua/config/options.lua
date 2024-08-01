aeb2be1dc2da53b34452550802dd90d7aa88c71e
Takuya Kosugiyama <re@itkq.jp>
2024-08-01 11:50:49 +0900

    nvim: autocmd

--- config/.config/nvim/lua/config/options.lua
+++ config/.config/nvim/lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.cmd([[
+  autocmd BufNewFile,BufRead *.go setl filetype=go tabstop=4 shiftwidth=4 noexpandtab
+]])
