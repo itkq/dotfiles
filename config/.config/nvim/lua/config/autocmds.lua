c1ebce9458cd80e9706d702728a7593c3af22736
Takuya Kosugiyama <re@itkq.jp>
2024-08-09 17:33:33 +0900

    nvim: auto readonly

--- config/.config/nvim/lua/config/autocmds.lua
+++ config/.config/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group = vim.api.nvim_create_augroup("ReadOnlyWarning", { clear = true }),
+  callback = function()
+    local first_line = vim.api.nvim_buf_get_lines(0, 0, 1, false)[1]
+    if first_line:match("DO NOT EDIT") then
+      vim.bo.readonly = true
+      vim.notify("This file is marked as DO NOT EDIT. Opened in read-only mode.", vim.log.levels.WARN)
+    end
+  end,
+})
